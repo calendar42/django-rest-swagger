4d0b6e0f8fb696f60b21f32970e88a530227abff
Bob Karreman <bob@karreman.org>
2015-05-12 16:09:24 +0200

    Also allow for excluding nested urls

--- rest_framework_swagger/views.py
+++ rest_framework_swagger/views.py
@@ -137,4 +137,4 @@ class SwaggerApiView(APIDocView):
     def get_api_for_resource(self, filter_path):
         urlparser = UrlParser()
         urlconf = getattr(self.request, "urlconf", None)
-        return urlparser.get_apis(urlconf=urlconf, filter_path=filter_path)
+        return urlparser.get_apis(urlconf=urlconf, filter_path=filter_path, exclude_namespaces=SWAGGER_SETTINGS.get('exclude_namespaces'))
