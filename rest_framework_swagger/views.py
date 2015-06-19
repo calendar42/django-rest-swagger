8d4288ba780d990765931f55992590afdf28afd5
Jasper Hartong <jasper@calendar42.com>
2015-06-19 15:31:52 +0200

    Fixed the patch for also excluding namespace within nested resources

--- rest_framework_swagger/views.py
+++ rest_framework_swagger/views.py
@@ -162,4 +162,4 @@ class SwaggerApiView(APIDocView):
     def get_api_for_resource(self, filter_path):
         urlparser = UrlParser()
         urlconf = getattr(self.request, "urlconf", None)
-        return urlparser.get_apis(urlconf=urlconf, filter_path=filter_path, exclude_namespaces=SWAGGER_SETTINGS.get('exclude_namespaces'))
+        return urlparser.get_apis(urlconf=urlconf, filter_path=filter_path, exclude_namespaces=rfs.SWAGGER_SETTINGS.get('exclude_namespaces'))
